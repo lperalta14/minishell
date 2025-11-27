c73618248be0eb43dc5419b8a53de3b7986e20d0
msedeno- <msedeno-@student.42malaga.com>
2025-11-27 11:09:36 +0100

    subo principio de fase token

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include "minishell.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
 
-	if (argc > 0 || argv || envp)
-	if (isatty(STDIN_FILENO))//para los test (isatty solo muestra el banner si es interactivo)
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	if (isatty (STDIN_FILENO)) //test(solo muestra banner si es interactivo)
 		print_banner("banners/acrobata.txt");
 	while (1)
 	{
@@ -14,13 +16,12 @@ int main(int argc, char **argv, char **envp)
 		{
 			if (isatty(STDIN_FILENO))
 				print_banner("banners/bye.txt");
-			break;;
+			break ;
 		}
 		add_history(input);
 		printf("%s\n", input);
 		free(input);
 	}
-	clear_history();
-	rl_clear_history();
-	return(0);
+	rl_clear_history(); //vers off +modern, cleanup neces, evita si mem issues,+simple y mantenible
+	return (0);
 }
