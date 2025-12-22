238731e244320678f37167b5ec0c11128417dbcd
msedeno- <msedeno-@student.42malaga.com>
2025-12-23 00:30:54 +0100

    23 ^b 25 00:30

--- include/builtins.h
+++ include/builtins.h
@@ -4,16 +4,16 @@
 # include "minishell.h"
 
 // Dispatcher
-int     is_builtin(char *cmd);
-int     execute_builtin(t_command *cmd, t_env **env);
+int	is_builtin(char *cmd);
+int	execute_builtin(t_command *cmd, t_env **env);
 
 // Builtins individuales
-int     ft_echo(char **args);
-int     ft_cd(char **args, t_env **env);
-int     ft_pwd(void);
-int     ft_export(char **args, t_env **env);
-int     ft_unset(char **args, t_env **env);
-int     ft_env(t_env *env);
-int     ft_exit(char **args);
+int	ft_echo(char **args);
+int	ft_cd(char **args, t_env **env);
+int	ft_pwd(void);
+int	ft_export(char **args, t_env **env);
+int	ft_unset(char **args, t_env **env);
+int	ft_env(t_env *env);
+int	ft_exit(char **args);
 
 #endif
