fd7b091ce49e551e70e37e0e88d713b6d35be4bf
msedeno- <msedeno-@student.42malaga.com>
2025-12-24 07:01:31 +0100

    24 ^b 25 07:01

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -23,22 +23,18 @@ int	ft_exit(char **args)
 	ft_putendl_fd("exit", 2);
 	if (!args || !args[1])
 		exit(g_exit_status);
-	if (args[1])
+	if (!is_numeric(args[1]))
 	{
-		if (!is_numeric(args[1]))
-		{
-			ft_putstr_fd("minishell: exit: ", 2);
-			ft_putstr_fd(args[1], 2);
-			ft_putendl_fd(": numeric argument required", 2);
-			exit(255);
-		}
-		if (args[2])
-		{
-			ft_putendl_fd("minishell: exit: too many arguments", 2);
-			return (1);
-		}
-		exit(ft_atoi(args[1]));
+		ft_putstr_fd("minishell: exit: ", 2);
+		ft_putstr_fd(args[1], 2);
+		ft_putendl_fd(": numeric argument required", 2);
+		exit(255);
 	}
-	exit(g_exit_status);
+	if (args[2])
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", 2);
+		return (1);
+	}
+	exit(ft_atoi(args[1]));
 	return (0);
 }
