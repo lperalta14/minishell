2392f0583b8c819deccfad8243a207d8330efd30
msedeno- <msedeno-@student.42malaga.com>
2026-01-03 21:33:59 +0100

    empiezo export sin terminar y sin verificar, pseudocodigo

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -1,2 +1,25 @@
 #include "../../include/minishell.h"
 
+int	is_valid_env_name(char *str)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (str[i])
+	{
+		c = str[i];
+		if (c == '=')
+			return (1);
+		if (i == 0 && ft_isdigit(c))
+			return (0);
+		if (!ft_isalnum(c) && c != '_')
+			return (0);
+		i++;
+	}
+}
+
+int	ft_export(char **args, char **env)
+{
+
+}
