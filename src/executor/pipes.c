4a5b19cb75c21e55e3bd5e0f3dedbf5b7db4a329
msedeno- <msedeno-@student.42malaga.com>
2026-01-11 23:21:12 +0100

    refactorizo pipes.c y añado funcion señales

--- src/executor/pipes.c
+++ src/executor/pipes.c
@@ -1,23 +1,67 @@
 #include "../../include/minishell.h"
 
+static void	wait_children(pid_t last_pid)
+{
+	pid_t	pid;
+	int		status;
+
+	while (1)
+	{
+		pid = wait(&status);
+		if (pid <= 0)
+			break ;
+		// Solo nos importa el status del último comando de la pipe
+		if (pid == last_pid)
+		{
+			if (WIFEXITED(status))
+				g_exit_status = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				g_exit_status = 128 + WTERMSIG(status);
+		}
+	}
+}
+
+static void	child_process(t_command *cmd, int prev_read, int *fd, t_env **env)
+{
+	if (prev_read != -1)
+	{
+		dup2(prev_read, STDIN_FILENO);
+		close(prev_read);
+	}
+	if (cmd->next)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+	}
+	execute_child(cmd, env);
+	exit(g_exit_status);
+}
+
+static void	parent_process(t_command *cmd, int *prev_read, int *fd)
+{
+	if (*prev_read != -1)
+		close(*prev_read);
+	if (cmd->next)
+	{
+		close(fd[1]);
+		*prev_read = fd[0];
+	}
+}
+
 void	execute_pipeline(t_command *cmd, t_env **env)
 {
-	int			prev_pipe_read;
-	t_command	*tmp;
+	int			prev_read;
 	int			fd[2];
 	pid_t		pid;
 
-	prev_pipe_read = -1;
-	tmp = cmd;
-	while (tmp)
+	prev_read = -1;
+	while (cmd)
 	{
-		if (tmp->next)
+		if (cmd->next && pipe(fd) == -1)
 		{
-			if (pipe(fd) == -1)
-			{
-				perror("minishell: pipe");
-				return ;
-			}
+			perror("minishell: pipe");
+			return ;
 		}
 		pid = fork();
 		if (pid == -1)
@@ -26,32 +70,9 @@ void	execute_pipeline(t_command *cmd, t_env **env)
 			return ;
 		}
 		if (pid == 0)
-		{
-			if (prev_pipe_read != -1) // vengo de un pipe anterior?
-			{
-				dup2(prev_pipe_read, STDIN_FILENO);
-				close(prev_pipe_read);
-			}
-			if (tmp->next)
-			{
-				close(fd[0]);
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[1]);
-			}
-			execute_child(tmp, env);
-		}
-		else
-		{
-			if (prev_pipe_read != -1)
-				close(prev_pipe_read);
-			if (tmp->next)
-			{
-				close(fd[1]);
-				prev_pipe_read = fd[0];
-			}
-			tmp = tmp->next;
-		}
+			child_process(cmd, prev_read, fd, env);
+		parent_process(cmd, &prev_read, fd);
+		cmd = cmd->next;
 	}
-	while (wait(NULL) > 0)
-		;
+	wait_children(pid);
 }
